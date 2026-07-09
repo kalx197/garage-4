74d4f55443a7299a1993e54c35794e3bb8fda253
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-09 15:58:48 -0600

    Delete config directory

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/garage_db'
-});
-
-module.exports = pool;
